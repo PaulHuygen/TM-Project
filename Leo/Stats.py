bf0d50e064a9b954a20cf3bd0a1a01862d90590f
Leonard Wein <leonardwein@gmail.com>
2013-06-05 22:41:43 +0200

    Updated all files and ordered directories
    also uploaded draft report and Eddie's world map

--- Leo/Stats.py
+++ Leo/Stats.py
@@ -14,18 +14,20 @@ if __name__ == '__main__' :
 
 	
 	def stats():
-		print 'Total number of tweets'
+		print 'Total number of tweets (1.788.070)'
 		print dbnp.tweets.find().count()
-		print 'Total number of retweeted tweets'
+		print 'Total number of retweeted tweets (757.950)'
 		print dbnp.tweets.find({'text' : {'$regex': u'^RT'}}).count()
-		print 'Number of valid tweets defined as containing text, language indication, and place information'
+		print 'Number of valid tweets defined as containing text, language indication, and place information (1.056.981)'
 		print dbnp.tweets.find({'text':{'$exists':True}, 'lang':{'$exists':True}, 'place':{'$exists':True}}).count()
-		print 'Number of Dutch language tweets in db'
+		print 'sames as above, but now requiring the fields to be non-empty (36.443)'
+		print dbnp.tweets.find({'text':{'$ne':None}, 'lang':{'$ne':None}, 'place':{'$ne':None}}).count()
+		print 'Number of Dutch language tweets in db (595.254)'
 		print dbnp.tweets.find({"lang": "nl"}).count()
-		print 'Number of English language tweets in db'
+		print 'Number of English language tweets in db (242.405)'
 		print dbnp.tweets.find({"lang": "en"}).count()
-		print 'Number of tweets in db without language indication'
-		print dbnp.tweets.find({"lang": "en"}).count()
+		print 'Number of tweets in db without language indication (731.089)'
+		print dbnp.tweets.find({"lang": None}).count()
 		print 'Language Stats'
 		print dbnp.tweets.aggregate([
 			{"$group": {"_id": "$lang", "count": {"$sum": 1}}},
@@ -38,10 +40,6 @@ if __name__ == '__main__' :
 		print dbnp.tweets.aggregate([
 					{"$group": {"_id": "$place.name", "count": {"$sum": 1}}},
 					{"$sort": SON([("count", -1), ("_id", -1)])}])['result']
-		print 'Number of Retweets'
-		print dbnp.tweets.find({'text' : {'$regex': u'RT'}}).count()
-
-
 
 
 
